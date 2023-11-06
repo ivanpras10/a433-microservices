621dd4af679e4eb1c735b105cb91430f018e3b70
Ivan Prastio <128587219+ivanpras10@users.noreply.github.com>
2023-11-06 11:04:50 +0700

    build_push_image_karsajobs-ui.sh

--- build_push_image_karsajobs-ui.sh
+++ build_push_image_karsajobs-ui.sh
@@ -1,14 +1,5 @@
-# untuk membuat Docker image dari Dockerfile dengan nama image item-app dan tag v1
-docker build -t item-app:v1 .
+docker build -t ivanprastio2/karsajobs:latest .
 
-# untuk menampilkan daftar image
-docker images
-
-# untuk mengubah nama image agar sesuai dengan format Docker Hub
-docker tag item-app:v1 ivanprastio2/item-app:v1
-
-# untuk login ke Docker Hub
 docker login -u ivanprastio2 --password 'ipancoy231'
 
-# untuk mengunggah image ke Docker Hub
-docker push ivanprastio2/item-app:v1
+docker push ivanprastio2/karsajobs:latest
