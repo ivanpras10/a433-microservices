5f2c9d50215d7a288aec23a4b9eb4c0e36f570e7
Ivan Prastio <128587219+ivanpras10@users.noreply.github.com>
2023-11-06 11:06:33 +0700

    build_push_image_karsajobs-ui.sh

--- build_push_image_karsajobs-ui.sh
+++ build_push_image_karsajobs-ui.sh
@@ -1,5 +1,5 @@
-docker build -t ivanprastio2/karsajobs:latest .
+docker build -t ivanprastio2/karsajobs-ui:latest .
 
 docker login -u ivanprastio2 --password 'ipancoy231'
 
-docker push ivanprastio2/karsajobs:latest
+docker push ivanprastio2/karsajobs-ui:latest
