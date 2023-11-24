ee36ec32cd3555e88208b26ad538956471b5cc9f
Ivan Prastio <128587219+ivanpras10@users.noreply.github.com>
2023-11-24 14:46:32 +0700

    build_push_image_shipping_service.sh

---
+++ build_push_image_shipping_service.sh
@@ -0,0 +1,8 @@
+# Perintah untuk membuat Docker image dengan format GitHub Packages.
+docker build -t ghcr.io/ivanpras10/shipping-service:latest .
+
+# Login ke GitHub Packages via Terminal.
+echo "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" | docker login ghcr.io -u ivanpras10 --password-stdin
+
+# Mengunggah image ke GitHub Packages.
+docker push ghcr.io/ivanpras10/shipping-service:latest
